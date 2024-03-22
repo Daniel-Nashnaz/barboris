36275588d39e604e68479574e0813a7b233ceac1
Daniel-Nashnaz <daniel.81953@gmail.com>
2024-03-22 02:58:20 +0200

    update barber route

--- src/routes/barber.route.ts
+++ src/routes/barber.route.ts
@@ -5,8 +5,3 @@ const prisma = new PrismaClient();
 
 export  {barberRoute}
 
-
-/*  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1",
-    "start": "ts-node-dev --inspect --transpile-only --ignore-watch node_modules --poll --respawn --exit-child src/index.ts"
-  }, */
