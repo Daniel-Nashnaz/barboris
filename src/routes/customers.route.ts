d2e5594d2e3f802685e78381c9bcf9e2b0fd6ade
Daniel Nassi <daniel.40768@gmail.com>
2024-05-06 01:16:34 +0300

    [MINOR] add try and catch

--- src/routes/customers.route.ts
+++ src/routes/customers.route.ts
@@ -1,12 +1,5 @@
 import { Router, Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
-const customersRoute = Router()
-const prisma = new PrismaClient();
-
- //res.status(201).json(task);
-
-
-// customers.controller.ts
 import {
   getAllCustomers,
   getCustomerById,
@@ -16,44 +9,57 @@ import {
 } from '../services/customers.service';
 import { CustomerDto } from '../model/customers.dto';
 
+const customersRoute = Router()
+const prisma = new PrismaClient();
 
+// get all customers from db 
 customersRoute.get('/customers', async (req: Request, res: Response) => {
   const customers = await getAllCustomers();
-  res.json(customers);
+  res.status(200).json(customers);
 });
 
+// get customer by id from db 
 customersRoute.get('/customers/:id', async (req: Request, res: Response) => {
-  const customerId = parseInt(req.params.id);
-  const customer = await getCustomerById(customerId);
-  res.json(customer);
+  try {
+    const customerId = parseInt(req.params.id);
+    const customer = await getCustomerById(customerId);
+    res.json(customer);
+  } catch (error: unknown) {
+    res.status(500).json({ message: 'Error fetching customer', error: (error as Error).message });
+}
+
 });
 
+// add customer of db 
 customersRoute.post('/customers', async (req: Request, res: Response) => {
   const customerData: CustomerDto = req.body;
-  
-  console.log(customerData);
   const newCustomer = await createCustomer(customerData);
   res.json(newCustomer);
 });
 
+// update customer in db 
 customersRoute.put('/customers/:id', async (req: Request, res: Response) => {
-  const customerId = parseInt(req.params.id);
-  const customerData: CustomerDto = req.body;
-  const updatedCustomer = await updateCustomer(customerId, customerData);
-  res.json(updatedCustomer);
+  try {
+    const customerId: number = parseInt(req.params.id);
+    const customerData: CustomerDto = req.body;
+    const updatedCustomer = await updateCustomer(customerId, customerData);
+    res.json(updatedCustomer);
+  } catch (error: unknown) {
+    res.status(500).json({ message: 'Error fetching customer', error: (error as Error).message });
+}
 });
 
+// delete customer in db 
 customersRoute.delete('/customers/:id', async (req: Request, res: Response) => {
+  try {
+    console.log("asasas");
+
   const customerId = parseInt(req.params.id);
   await deleteCustomer(customerId);
-  res.sendStatus(204);
+  res.sendStatus(204).json(`delete customer ${customerId} successfully`);
+  } catch (error: unknown) {
+    res.status(500).json({ message: 'Error fetching customer', error: (error as Error).message });
+}
 });
 
-
-export  {customersRoute}
-
-
-/*  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1",
-    "start": "ts-node-dev --inspect --transpile-only --ignore-watch node_modules --poll --respawn --exit-child src/index.ts"
-  }, */
+export { customersRoute }
--- src/services/customers.service.ts
+++ src/services/customers.service.ts
@@ -1,6 +1,7 @@
 // customers.service.ts
 import { PrismaClient } from '@prisma/client';
 import { CustomerDto } from '../model/customers.dto';
+import { isNullOrUndefined } from 'util';
 
 const prisma = new PrismaClient();
 
@@ -9,9 +10,17 @@ export const getAllCustomers = async () => {
 };
 
 export const getCustomerById = async (customerId: number) => {
-  return prisma.customers.findUnique({
-    where: { id: customerId },
-  });
+ try {
+    const user = await prisma.customers.findUnique({
+      where: { id: customerId },
+    });
+    if (isNullOrUndefined(user)) {
+      throw new Error(`Customer ${customerId} not found`);
+    }
+    return user;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const createCustomer = async (customerData: CustomerDto) => {
@@ -21,18 +30,60 @@ export const createCustomer = async (customerData: CustomerDto) => {
 };
 
 export const updateCustomer = async (customerId: number, customerData: CustomerDto) => {
-  const updatedCustomerData = {
+ /* const updatedCustomerData = {
     ...customerData,
-    updated_at: new Date() 
+    updated_at: new Date()
   };
   return prisma.customers.update({
     where: { id: customerId },
     data: updatedCustomerData,
   });
+*/
+
+  try {
+    const existingCustomer = await prisma.customers.findUnique({
+      where: { id: customerId },
+    });
+
+    if (!existingCustomer) {
+      // Handle the case where the customer is not found in the database
+      // For example, you could choose to create a new customer entry
+      // Or log a message and return null
+      throw new Error(`Customer with ID ${customerId} not found`);
+    }
+
+    const updatedCustomerData = {
+      ...customerData,
+      updated_at: new Date()
+    };
+
+    const updatedCustomer = await prisma.customers.update({
+      where: { id: customerId },
+      data: updatedCustomerData,
+    });
+
+    return updatedCustomer;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const deleteCustomer = async (customerId: number) => {
-  return prisma.customers.delete({
-    where: { id: customerId },
-  });
+  try {
+    const deletedCustomer = await prisma.customers.delete({
+      where: { id: customerId },
+    });
+    console.log(deletedCustomer);
+
+    if (isNullOrUndefined(deletedCustomer)) {
+      // Handle the case where the customer is not found in the database
+      // For example, you could choose to create a new customer entry
+      // Or log a message and return null
+      console.log("asasas");
+      throw new Error(`Customer with ID ${customerId} not found`);
+    }
+    return deletedCustomer;
+  } catch (error) {
+    throw error;
+  }
 };
