01f9e95524d4bde4d549140471b00d64ca6a8d11
Daniel-Nashnaz <daniel.81953@gmail.com>
2024-05-20 23:49:28 +0300

    [MINOR] update endpoint getAppointmentsAvailable

--- src/routes/appointments.route.ts
+++ src/routes/appointments.route.ts
@@ -5,6 +5,7 @@ import {
   createAppointment,
   updateAppointment,
   deleteAppointment,
+  getAppointmentsAvailable,
   getAppointmentsForDate,
 } from '../services/appointments.service';
 import { PrismaClient } from '@prisma/client';
@@ -21,16 +22,17 @@ appointmentRoute.get('/appointments', async (req: Request, res: Response) => {
     res.status(500).json({ error: (error as Error).message });
   }
 });
-appointmentRoute.get('/appointments/test', async (req: Request, res: Response) => {
+
+appointmentRoute.get('/appointmentsByDate', async (req: Request, res: Response) => {
   try {
-    console.log("asas");
-    const appointments = await getAppointmentsForDate(req.body.date);
+    // Check if the date is of type string. If it is, use it; otherwise, use undefined
+    const date = typeof req.query.data === 'string' ? req.query.data : undefined;
+    const appointments = await getAppointmentsForDate(date);
     res.json(appointments);
   } catch (error) {
     res.status(500).json({ error: (error as Error).message });
   }
 });
-
 
 appointmentRoute.get('/appointments/:id', async (req: Request, res: Response) => {
   const { id } = req.params;
@@ -56,203 +58,29 @@ appointmentRoute.post('/appointments', async (req: Request, res: Response) => {
   }
 });
 
-const appointme : Appointmen[] = [
-  {
-    appointment_time_start: "2024-05-14T18:00:00.000Z",
-    appointment_time_end: "2024-05-14T18:30:00.000Z"
+// const appointme : Appointmen[] = [
+//   {
+//     appointment_time_start: "2024-05-14T18:00:00.000Z",
+//     appointment_time_end: "2024-05-14T18:30:00.000Z"
 
-  }
-  ,
-  {
-    appointment_time_start: "2024-05-15T12:00:00.000Z",
-    appointment_time_end: "2024-05-14T12:30:00.000Z",
-  }
-  ,
-  {
-    appointment_time_start: "2024-05-15T15:00:00.000Z",
-    appointment_time_end: "2024-05-14T15:30:00.000Z",
-  }
-];
+//   }
+//   ,
+//   {
+//     appointment_time_start: "2024-05-15T12:00:00.000Z",
+//     appointment_time_end: "2024-05-14T12:30:00.000Z",
+//   }
+//   ,
+//   {
+//     appointment_time_start: "2024-05-15T15:00:00.000Z",
+//     appointment_time_end: "2024-05-14T15:30:00.000Z",
+//   }
+// ];
 
-
-/*
-function generateTimeSlots(startTime:any, endTime: any  ) {
-  const timeSlots = [];
-  let currentTime = new Date(startTime);
-
-  // Convert start and end time strings to Date objects
-  const startDate = new Date(startTime);
-  const endDate = new Date(endTime);
-
-  // Loop until current time is less than end time
-  while (currentTime < endDate) {
-      // Add current time to time slots if it's not within any appointment
-      const isWithinAppointment = appointments.some(appointmen => {
-          const appointmentStart = new Date(appointmen.appointment_time_start);
-          const appointmentEnd = new Date(appointmen.appointment_time_end);
-          return currentTime >= appointmentStart && currentTime < appointmentEnd;
-      }) 
-      if (!isWithinAppointment) {
-        const hours = currentTime.getHours().toString().padStart(2, '0');
-        const minutes = currentTime.getMinutes().toString().padStart(2, '0');
-        const timeString = `${hours}:${minutes}`;
-        timeSlots.push(timeString);
-    }
-
-      // Move to the next time slot (30 minutes later)
-      currentTime = new Date(currentTime.getTime() + 30 * 60 * 1000);
-  }
-
-  return timeSlots;
-}
-
-async function getAppointmentsOnDateTime(dateTime:any) {
-    try {
-        const date = new Date(dateTime);
-        const startOfDay = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-        const endOfDay = new Date(startOfDay.getTime() + 24 * 60 * 60 * 1000); // Next day
-
-        const appointments = await prisma.appointments.findMany({
-            where: {
-                AND: [
-                    {
-                        appointment_time_start: {
-                            gte: startOfDay,
-                        },
-                    },
-                    {
-                        appointment_time_start: {
-                            lt: endOfDay,
-                        },
-                    },
-                ],
-            },
-            select: {
-              appointment_time_start: true,
-          },
-        });
-        return appointments;
-    } catch (error) {
-        console.error("Error fetching appointments:", error);
-        throw error;
-    }
-}
-*/
-interface Appointmen {
-  appointment_time_start: string;
-  appointment_time_end: string;
-}
-interface Appointment {
-  appointment_time_start: Date;
-  appointment_time_end: Date;
-}
-/*
-const generateTimeSlots = async (startTime: string, endTime: string, appointments: Appointment[]): Promise<string[]> => {
-  const timeSlots = [];
-  let currentTime = new Date(startTime);
-  // Convert start and end time strings to Date objects
-  const endDate = new Date(endTime);
-  // Loop until current time is less than end time
-  while (currentTime < endDate) {
-    // Add current time to time slots if it's not within any appointment
-    const isWithinAppointment = appointments.some(appointment => {
-      const appointmentStart = new Date(appointment.appointment_time_start);
-      const appointmentEnd = new Date(appointment.appointment_time_end);
-      return currentTime >= appointmentStart && currentTime < appointmentEnd;
-    })
-    if (!isWithinAppointment) {
-      const hours = currentTime.getHours().toString().padStart(2, '0');
-      const minutes = currentTime.getMinutes().toString().padStart(2, '0');
-      const timeString = `${hours}:${minutes}`;
-      timeSlots.push(timeString);
-    }
-
-    // Move to the next time slot (30 minutes later)
-    currentTime = new Date(currentTime.getTime() + 30 * 60 * 1000);
-  }
-
-  return timeSlots;
-}
-*/
-
-
-const generateTimeSlots = async (startTime: Date, endTime: Date, appointments: Appointmen[]): Promise<string[]> => {
-  const timeSlots: string[] = [];
-  const timeIncrement: number = 30; 
-  let currentTime: Date = new Date(startTime);
-  const endDateTime: Date = new Date(endTime);
-
-  const getHourAndMinute = (date: Date): string => {
-      const isoString: string = date.toISOString();
-      return isoString.slice(11, 16); 
-  };
-
-  const appointmentStartTimes: Set<string> = new Set(appointments.map(appointment => getHourAndMinute(new Date(appointment.appointment_time_start))));
-
-  while (currentTime <= endDateTime) {
-      const timeString: string = getHourAndMinute(currentTime);
-
-      if (!appointmentStartTimes.has(timeString)) {
-          timeSlots.push(timeString);
-      }
-
-      currentTime = new Date(currentTime.getTime() + timeIncrement * 60 * 1000); 
-  }
-
-  return timeSlots;
-};
-
-
-
-const getAppointmentsOnDateTime = async (dateTime: string): Promise<Appointment[]> => {
-
-  try {
-    const date = new Date(dateTime);
-    const startOfDay = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-    const endOfDay = new Date(startOfDay.getTime() + 24 * 60 * 60 * 1000); // Next day
-
-    const appointments = await prisma.appointments.findMany({
-      where: {
-        AND: [
-          {
-            appointment_time_start: {
-              gte: startOfDay,
-            },
-          },
-          {
-            appointment_time_start: {
-              lt: endOfDay,
-            },
-          },
-        ],
-      },
-      select: {
-        appointment_time_start: true,
-        appointment_time_end: true,
-      },
-    });
-    return appointments;
-  } catch (error) {
-    console.error("Error fetching appointments:", error);
-    throw error;
-  }
-}
 appointmentRoute.get('/availableSlots/:date', async (req: Request, res: Response) => {
   try {
     const { date } = req.params;
-    //const appointments = await getAppointmentsOnDateTime(date);
-
-    //const availableSlots = getAvailableSlots(date);
-    //res.json(appointments);
-
-    // Example usage:
-    const startTime = "2024-05-20T08:00:00Z"; // Example start time
-    const endTime = "2024-05-20T17:00:00Z"; // Example end time
-    //const appointments = await getAppointmentsOnDateTime(date); // Fetch appointments for the day
-   // console.log(appointme);
-    const timeSlots = await generateTimeSlots(new Date(startTime), new Date(endTime), appointme);
-    res.json(timeSlots);
-
+    const appointmentsAvailable = await getAppointmentsAvailable(date);
+    res.json(appointmentsAvailable);
   } catch (error) {
     res.status(500).json({ error: (error as Error).message });
   }
--- src/services/appointments.service.ts
+++ src/services/appointments.service.ts
@@ -1,8 +1,10 @@
 import { PrismaClient, appointments } from '@prisma/client';
-import { AppointmentDto } from '../model/appointment.dto';
 import { AppointmentDetails } from '../model/AppointmentDetails.dto';
+import { AppointmentDto } from '../model/appointment.dto';
 
 const prisma = new PrismaClient();
+const startTime:Date = new Date("2024-05-20T08:00:00Z");
+const endTime:Date = new Date("2024-05-20T17:00:00Z");
 
 export const getAllAppointments = async (): Promise<appointments[]> => {
     try {
@@ -13,7 +15,8 @@ export const getAllAppointments = async (): Promise<appointments[]> => {
     }
 };
 
-export const getAppointmentsForDate = async (dateString: string = new Date().toISOString()): Promise<AppointmentDetails[]> => {
+export const getAppointmentsForDate = async (dateString: string  = new Date().toLocaleString()): Promise<AppointmentDetails[]> => {
+    console.log(dateString);
     const today = new Date(dateString);
     today.setHours(0, 0, 0, 0);
     const tomorrow = new Date(today);
@@ -45,7 +48,8 @@ export const getAppointmentsForDate = async (dateString: string = new Date().toI
 
         return formattedAppointments;
     } catch (error) {
-        throw new Error(`Error fetching appointments for the specified date: ${error}`);
+        console.error("Error fetching appointments:", error);
+        throw new Error(`Error fetching appointments: ${error}`);
     }
 };
 
@@ -56,6 +60,13 @@ export const getAppointmentById = async (id: number): Promise<appointments | nul
     } catch (error) {
         throw new Error(`Error fetching appointment by ID: ${error}`);
     }
+};
+
+export const getAppointmentsAvailable = async(dateTime: string) => {
+    const appointments: AppointmentDetails[] = await getAppointmentsForDate(dateTime);
+    const timeSlots: string[] = await generateTimeSlots(startTime,endTime, appointments);
+    return timeSlots;
+    
 };
 
 export const createAppointment = async (appointment: AppointmentDto): Promise<appointments> => {
@@ -88,4 +99,63 @@ export const deleteAppointment = async (id: number): Promise<appointments | null
     }
 };
 
+/*
+const getAppointmentsOnDateTime = async (dateTime: string | Date): Promise<Appointment[]> => {
 
+    try {
+        const date = new Date(dateTime);
+        const startOfDay = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+        const endOfDay = new Date(startOfDay.getTime() + 24 * 60 * 60 * 1000); // Next day
+
+        const appointments = await prisma.appointments.findMany({
+            where: {
+                AND: [
+                    {
+                        appointment_time_start: {
+                            gte: startOfDay,
+                        },
+                    },
+                    {
+                        appointment_time_start: {
+                            lt: endOfDay,
+                        },
+                    },
+                ],
+            },
+            select: {
+                appointment_time_start: true,
+                appointment_time_end: true,
+            },
+        });
+        return appointments;
+    } catch (error) {
+        console.error("Error fetching appointments:", error);
+        throw error;
+    }
+};
+*/
+
+const generateTimeSlots = async (startTime: Date, endTime: Date, appointments: AppointmentDetails[], timeIncrement: number | undefined = 30): Promise<string[]> => {
+    const timeSlots: string[] = [];
+    let currentTime: Date = new Date(startTime);
+    const endDateTime: Date = new Date(endTime);
+
+    const appointmentStartTimes: Set<string> = new Set(appointments.map(appointment => getHourAndMinute(new Date(appointment.appointment_time_start))));
+
+    while (currentTime <= endDateTime) {
+        const timeString: string = getHourAndMinute(currentTime);
+
+        if (!appointmentStartTimes.has(timeString)) {
+            timeSlots.push(timeString);
+        }
+
+        currentTime = new Date(currentTime.getTime() + timeIncrement * 60 * 1000);
+    }
+
+    return timeSlots;
+};
+
+const getHourAndMinute = (date: Date): string => {
+    const isoString: string = date.toISOString();
+    return isoString.slice(11, 16);
+};
